a7892ca18e415e12d562e647f1290836f2286f47
tecchu11 <agdjmp.ajtt@gmail.com>
2024-02-03 00:47:13 +0900

    foo

--- domain/main.tf
+++ domain/main.tf
@@ -1,24 +1,3 @@
-module "sg" {
-  source      = "../module/sg"
-  description = "sample sg"
-  name        = "sample-sg"
-  vpc_id      = "sample-vpc-id"
-  ingress_rules = {
-    sample = {
-      cidr_ipv4   = "0.0.0.0/0"
-      description = "sample sg rule"
-      from_port   = 443
-      ip_protocol = "tcp"
-      to_port     = 8080
-    }
-  }
-  egress_rules = {
-    full_open = {
-      cidr_ipv4   = "0.0.0.0/0"
-      description = "full open"
-      from_port   = 0
-      ip_protocol = "-1"
-      to_port     = 0
-    }
-  }
+resource "aws_s3_bucket" "example" {
+  bucket = "foo"
 }
